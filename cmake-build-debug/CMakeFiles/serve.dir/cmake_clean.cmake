479aa48d508867c31cdfd2311eb9cc83ceeacbab
Esteban Lanter <estebanlanter@Estebans-MacBook-Pro.local>
2025-07-14 09:55:34 -0500

    dyf

--- cmake-build-debug/CMakeFiles/serve.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/serve"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/serve.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
